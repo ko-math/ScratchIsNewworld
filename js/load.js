51cb5dffee95b774dcd69a8b41cfc7ce4aaae5ba
ko-math <sorehakiemasu@gmail.com>
2026-03-21 17:23:01 +0900

    Create load.js

---
+++ js/load.js
@@ -0,0 +1,9 @@
+const el = document.querySelector('.circle1pa');
+
+el.addEventListener('animationstart' function (){
+  console.log('開始');
+});
+
+el.addEventListener('animationend', function () {
+  console.log('終了');
+});
